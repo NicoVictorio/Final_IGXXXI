1271746848e8356582e9920be0c80a84d1d5dc00
gededrm <114561837+gede-drm@users.noreply.github.com>
2023-09-09 13:00:14 +0700

    ESA-Fixed GetTier Bug

--- app/Http/Controllers/ShippingAgentController.php
+++ app/Http/Controllers/ShippingAgentController.php
@@ -196,7 +196,7 @@ class ShippingAgentController extends Controller
             $cekNomor += 2;
         }
 
-        $cekBayangan = ShippingContainer::select('isa_due_date')->where('ica_target_bay', $bay)->where('ica_target_row', $row)->where('ica_target_tier', $cekNomor)->where('Team_id', $idTeam)->where('Period_id', 1)->count();
+        $cekBayangan = ShippingContainer::select('isa_due_date')->where('isa_due_date', $bay)->where('ica_target_row', $row)->where('ica_target_tier', $cekNomor)->where('Team_id', $idTeam)->where('Period_id', 1)->count();
         if($cekBayangan == 1){
             $cekNomor += 2;
         }
