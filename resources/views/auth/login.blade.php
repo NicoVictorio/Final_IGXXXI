1450ad22bd95da45541ac36ad4b9e3e6e5c718e0
160421115 <s160421115@student.ubaya.ac.id>
2023-09-09 11:10:47 +0700

    login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,34 +2,47 @@
 
 @section('content')
     <link href="{{ asset('css/login.css') }}" rel="stylesheet">
-    <section>
-        <div class="login-box">
-            <form method="POST" action="{{ route('login') }}">
-                @csrf
-                <h2>Login</h2>
-                @if ($errors->any())
-                    <div class="alert-box">
-                        <div class="alert alert-danger" role="alert">
-                            <p class="mb-0"><strong>Oops!</strong></p>
-                            @foreach ($errors->all() as $error)
-                                <p class="mt-0 mb-1 fs-5">{{ $error }}</p>
-                            @endforeach
+    <body>
+        <div class="container">
+            {{-- Logo IG --}}
+            <div class="row">
+                <div class="col-12 mt-3 d-flex flex-wrap justify-content-center">
+                    <img class="fix-image headline" src="{{ asset('assets') }}/ig-logo.png"
+                        style="max-height: 250px;">
+                </div>
+            </div>
+            {{-- End Of Logo IG --}}
+            <br><br>
+            <div class="row justify-content-center">
+                <div class="login-box">
+                    <form method="POST" action="{{ route('login') }}">
+                        @csrf
+                        <h2>Login</h2>
+                        @if ($errors->any())
+                            <div class="alert-box">
+                                <div class="alert alert-danger" role="alert">
+                                    <p class="mb-0"><strong>Oops!</strong></p>
+                                    @foreach ($errors->all() as $error)
+                                        <p class="mt-0 mb-1 fs-5">{{ $error }}</p>
+                                    @endforeach
+                                </div>
+                            </div>
+                        @endif
+                        <div class="input-box">
+                            <span class="icon"><ion-icon name="mail"></ion-icon></span>
+                            <input name="username" required>
+                            <label>Username</label>
                         </div>
-                    </div>
-                @endif
-                <div class="input-box">
-                    <span class="icon"><ion-icon name="mail"></ion-icon></span>
-                    <input name="username" required>
-                    <label>Username</label>
+                        <div class="input-box">
+                            <span class="icon"><ion-icon name="lock-closed"></ion-icon></span>
+                            <input type="password" name="password" required>
+                            <label>Password</label>
+                        </div>
+                        <button type="submit">Login</button>
+                    </form>
                 </div>
-                <div class="input-box">
-                    <span class="icon"><ion-icon name="lock-closed"></ion-icon></span>
-                    <input type="password" name="password" required>
-                    <label>Password</label>
-                </div>
-                <button type="submit">Login</button>
-            </form>
+            </div>
         </div>
-    </section>
+    </body>
 
 @endsection
